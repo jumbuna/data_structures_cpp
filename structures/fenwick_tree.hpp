3b9a1ec156b743116110dad403dc0270d8ce9fbd
jumbuna <jacobwangoni@yahoo.com>
2020-06-02 21:48:39 +0300

    converted fenwick tree to generic class

--- structures/fenwick_tree.hpp
+++ structures/fenwick_tree.hpp
@@ -8,56 +8,61 @@
 
 #include <iostream>
 
+template <class T>
 struct fenwicktree {
-    fenwicktree(int*, int);
+    fenwicktree(T*, int);
     ~fenwicktree();
     int leastsignificantbit(int&);
-    int sum(int, int);
-    void update(int, int);
+    T sum(int, int);
+    void update(int, T);
     void reset();
 private:
-    int* _array;
-    int* _oldarray;
+    T* _array;
+    T* _oldarray;
     int _size;
-    int _sum(int);
+    T _sum(int);
 };
 
-fenwicktree::fenwicktree(int* arr, int size)
+template <class T>
+fenwicktree<T>::fenwicktree(T* arr, int size)
 :_size(size+1)
 {
     _oldarray = arr;
-    _array = new int[_size];
+    _array = new T[_size]; //use std::calloc which initializes all element to 0
     for(int i = 0; i <= _size; i++)
         _array[i] = arr[i-1];
     for(int i = 1; i <= _size; i++) {
         int parent = i + leastsignificantbit(i);
-        if(parent < _size) {
+        if(parent < _size)
             _array[parent] += _array[i];
-        }
     }
 }
-
-fenwicktree::~fenwicktree() {
+template <class T>
+fenwicktree<T>::~fenwicktree() {
     delete [] _array;
 }
 
-void fenwicktree::reset() {
+template <class T>
+void fenwicktree<T>::reset() {
     for(int i = 1; i < _size; i++) {
         _array[i] = 0;
     }
 }
 
-int fenwicktree::leastsignificantbit(int& _i) {
+template <class T>
+int fenwicktree<T>::leastsignificantbit(int& _i) {
     return (_i & (_i*-1));
 }
 
-int fenwicktree::sum(int _start, int _stop) {
+template <class T>
+T fenwicktree<T>::sum(int _start, int _stop) {
     
     return _sum(_stop) - _sum(--_start);
 }
 
-int fenwicktree::_sum(int _index) {
-    int sum = 0;
+template <class T>
+T fenwicktree<T>::_sum(int _index) {
+    T sum = 0;
     sum += _array[_index];
     while(_index > 0) {
         _index -= leastsignificantbit(_index);
@@ -66,8 +71,9 @@ int fenwicktree::_sum(int _index) {
     return sum;
 }
 
-void fenwicktree::update(int _index, int _value) {
-    int incremental = _value - _oldarray[_index-1];
+template <class T>
+void fenwicktree<T>::update(int _index, T _value) {
+    T incremental = _value - _oldarray[_index-1];
     _array[_index]+= incremental;
     while(_index < _size) {
         _index += leastsignificantbit(_index);
--- structures/main.cpp
+++ structures/main.cpp
@@ -10,7 +10,7 @@
 #include "fenwick_tree.hpp"
 
 int main() {
-    int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
-    fenwicktree fenwick(array, 30);
+    double array[] = {1.0, 2.6, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
+    fenwicktree<double> fenwick(array, 30);
     std::cout << fenwick.sum(1, 30) << std::endl;
 }
